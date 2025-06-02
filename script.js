5370d69d33ef6ae2164a0c587b457ab3a9483be1
Sajad <sajadsoorni@gmail.com>
2025-06-02 15:43:29 +0100

    Fix initial page load bug in reveal-on-scroll logic

--- script.js
+++ script.js
@@ -176,12 +176,12 @@ headerObserver.observe(header);
 const allSections = document.querySelectorAll('section');
 
 const revelSection = function (entries, observer) {
-  const [entry] = entries;
-  console.log(entry);
-
-  if (!entry.isIntersecting) return;
-  entry.target.classList.remove('section--hidden');
-  observer.unobserve(entry.target);
+  // console.log(entries);
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) return;
+    entry.target.classList.remove('section--hidden');
+    observer.unobserve(entry.target);
+  });
 };
 
 const sectionObserver = new IntersectionObserver(revelSection, {
